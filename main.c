df4673dddc18de4377c561404ca6faeb768a006d
Marine BARON <mbaron@student.42.fr>
2018-01-10 16:32:39 +0100

    change main with new variables manes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mbaron <mbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 10:02:11 by mbaron            #+#    #+#             */
-/*   Updated: 2018/01/08 18:10:11 by mbaron           ###   ########.fr       */
+/*   Updated: 2018/01/10 16:24:28 by mbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,22 @@
 int		main(int argc, char *argv[])
 {
 	t_tetra			tetras_lib[TETRAS_LIB_NB];
-	short int		*tetras;
-	int				tetras_length;
-	char			grid[GRID_MAX][GRID_MAX];
-	int				grid_length;
+	t_tetra			**tetras;
+	int				tetras_nb;
+	t_bt			**bt;
+	int				grid_size;
 
 	if (!test_params(argc))
 		return (put_error(0));
 	init_tetras_lib(tetras_lib);
 	tetras = NULL;
-	tetras_length = test_source(argv[1], tetras_lib, tetras);
-	if (tetras_length == -1)
+	tetras_nb = test_source(argv[1], tetras_lib, tetras);
+	if (tetras_nb == -1)
 		return (put_error(1));
-	grid_length = get_grid(tetras, tetras_length, grid);
-	if (grid_length == -1)
+	bt = NULL;
+	grid_size = solver(tetras, tetras_nb, bt);
+	if (grid_size == -1)
 		return (put_error(1));
-	put_grid(grid, grid_length);
+	put_grid(bt, grid_size);
 	return (0);
 }
