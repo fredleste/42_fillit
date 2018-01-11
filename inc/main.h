f262d5e67f5e2cf3d9733e404a8961e36109dd57
Marine BARON <mbaron@student.42.fr>
2018-01-11 13:15:01 +0100

    all files width pieces -------------  - add define PIECES_NB_MAX

--- inc/main.h
+++ inc/main.h
@@ -6,7 +6,7 @@
 /*   By: mbaron <mbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 10:03:55 by mbaron            #+#    #+#             */
-/*   Updated: 2018/01/11 12:10:26 by fleste-l         ###   ########.fr       */
+/*   Updated: 2018/01/11 13:12:18 by mbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include "tetras_lib.h"
 # define GRID_MAX 16
 # define TETRAS_LIB_NB 19
+# define PIECES_NB_MAX 26
 # define FILLIT_USAGE "usage: ./fillit source_file\n"
 
 typedef struct	s_piece
@@ -27,8 +28,8 @@ typedef struct	s_piece
 }				t_piece;
 void 	init_tetras_lib	(t_tetra tetras_lib[]);
 int 	test_params(int argc);
-int 	test_source(char *file_name, t_tetra tetras_lib[], t_piece *pieces);
-int		solver(t_piece *pieces, int pieces_nb, t_list **bt);
+int 	test_source(char *file_name, t_tetra tetras_lib[], t_piece pieces[]);
+int		solver(t_piece pieces[], int pieces_nb, t_list **bt);
 int		put_error(int error);
 int		put_error_log(const char *str);
 int		put_grid(t_list **bt, int pieces_nb, int grid_size);
