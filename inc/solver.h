163bd83252129190472a82430c3bc21257b157f9
Marine BARON <mbaron@student.42.fr>
2018-01-12 10:32:07 +0100

    forget solver.h (oups !)

--- inc/solver.h
+++ inc/solver.h
@@ -6,7 +6,7 @@
 /*   By: fleste-l <fleste-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/11 18:56:40 by fleste-l          #+#    #+#             */
-/*   Updated: 2018/01/12 09:57:25 by mbaron           ###   ########.fr       */
+/*   Updated: 2018/01/12 10:30:55 by mbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define SOLVER_H
 # define GRID_ALL_ONE 0xFFFF
 
-int				add_piece(t_piece *piece, unsigned char *bt_size, t_list **p_bt);
+int				solver_add_piece(t_piece *piece, unsigned char *bt_size, t_list **p_bt);
 unsigned char	calc_grid_size(int pieces_nb);
 int 			solver_write_grid(unsigned short int grid[],
 	unsigned char grid_size);
