c54237c7d34a2e7be5e3be979788ba485c99d4da
Frederic LESTE-LASSERRE <fleste-l@student.42.fr>
2018-01-15 18:43:59 +0100

    tests work in progress

--- test.c
+++ test.c
@@ -6,14 +6,14 @@
 /*   By: mbaron <mbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 11:39:33 by mbaron            #+#    #+#             */
-/*   Updated: 2018/01/12 19:20:10 by fleste-l         ###   ########.fr       */
+/*   Updated: 2018/01/15 18:41:20 by fleste-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "inc/main.h"
-#include "inc/test.h"
-#include "inc/tetras_lib.h"
-#include "inc/output.h"
+#include "main.h"
+#include "test.h"
+#include "tetras_lib.h"
+#include "output.h"
 
 int		test_params(int argc)
 {
@@ -26,47 +26,47 @@ int		put_file(char *file_name, char *str_pieces)
 	int		n;
 	int		i;
 	int		j;
-	char	str[BUF_SIZE + 1];
+	char	buf[BUF_SIZE + 1];
 
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
 	{
-		put_error_log("Error open file");
+		put_error_log("Error open file\n");
 		return (-1);
 	}
-	n = read(fd, str, BUF_SIZE);
-	str[n] = '\0';
+	n = read(fd, buf, BUF_SIZE);
+	buf[n] = '\0';
 	if (close(fd) == -1)
 	{
-		put_error_log("Error close file");
+		put_error_log("Error close file\n");
 		return (-1);
 	}
 	if ((n + 1) % 21 != 0)
 	{
-		put_error_log("Error wrong file");
+		put_error_log("Error wrong file\n");
 		return (-1);
 	}
 	i = 0;
 	j = 0;
 	while (i < n)
 	{
-		if (i % 5 == 0 || i % 21 == 0)
+		if ((i + 1 - (i / 21)) % 5 == 0 || (i + 1) % 21 == 0)
 		{
-			if (str[i] != '\n')
+			if (buf[i] != '\n')
 			{
-				put_error_log("Error wrong tetra in file");
+				put_error_log("Error wrong file format\n");
 				return (-1);
 			}
 		}
 		else
-			str_pieces[j++] = str[i];
+			str_pieces[j++] = buf[i];
 		i++;
 	}
 	str_pieces[j] = '\0';
 	return ((n + 1) / 21);
 }
 
-int		put_binary(char *str)
+t_tetra	*put_binary(char *str, t_tetra tetras_lib[])
 {
 	int				i;
 	unsigned short	n;
@@ -76,57 +76,51 @@ int		put_binary(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] == '#')
-		{
 			n = n | (1 << (15 - i));
-		}
+		else if (str[i] != '.')
+			return (NULL);
 		i++;
 	}
 	while ((n & 0x8888) == 0)
 		n = n << 1;
 	while ((n & 0xf000) == 0)
 		n = n << 4;
-	return (n);
-}
-
-int		put_pieces(char *str_pieces, t_tetra tetras_lib[], int pieces_nb,
-	t_piece pieces[])
-{
-	int				i;
-	int				j;
-	char			str[16];
-	unsigned short	bin_piece;
-
-	while (i < pieces_nb)
-	{
-		while ()
-		bin_piece = put_binary(str, tetras_lib);
-		i = 0;
-		while (n != tetras_lib[i].n)
-			i++;
-		if (i == TETRAS_LIB_NB)
-		{
-			put_error_log("Error wrong binary piece");
-			return (-1);
-		}
-	}
-	return (1);
+	i = 0;
+	while (i < TETRAS_LIB_NB && n != tetras_lib[i].n)
+		i++;
+	if (i == TETRAS_LIB_NB)
+		return (NULL);
+	return (&(tetras_lib[i]));
 }
 
 int		test_source(char *file_name, t_tetra tetras_lib[], t_piece pieces[])
 {
 	int		i;
+	int		n;
 	int		pieces_nb;
+	char	str[16];
 	char	str_pieces[BUF_SIZE + 1];
 
 	pieces_nb = put_file(file_name, str_pieces);
-	i = 0;
-	while (i < pieces_nb)
+	n = 0;
+	while (n < pieces_nb)
 	{
-		pieces[i].tetra = &(tetras_lib[i]);
-		pieces[i].l = -1;
-		pieces[i].c = -1;
-		i++;
+		i = 0;
+		while (i < 16)
+		{
+			str[i] = str_pieces[(n * 16) + i];
+			i++;
+		}
+		str[i] = '\0';
+		pieces[n].tetra = put_binary(str, tetras_lib);
+		if (pieces[n].tetra == NULL)
+		{
+			put_error_log("Error wrong piece\n");
+			return (-1);
+		}
+		pieces[n].l = -1;
+		pieces[n].l = -1;
+		n++;
 	}
-	put_pieces(str_pieces, tetras_lib, pieces_nb, pieces);
 	return (pieces_nb);
 }
